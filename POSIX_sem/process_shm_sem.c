9cdb62c303e85c6b633709d3c55d1ba1f622f43e
cityhonor <794411923@qq.com>
2023-02-18 23:18:23 +0800

    修复停止程序后没释放资源的问题

--- POSIX_sem/process_shm_sem.c
+++ POSIX_sem/process_shm_sem.c
@@ -24,6 +24,55 @@ static sem_t    *sem_lock_client = NULL;
 static sem_t    *sem_lock_server = NULL;
 static void     *shm_addr = NULL;
 static int      shmid = -1;
+static int      exit_cnt = 0;
+
+void eixt_handle(int signal)
+{
+    int status;
+    if(signal == SIGINT || signal == SIGTERM)
+    {
+        exit_cnt++;
+        printf("eixt_handle running exit_cnt = %d\r\n", exit_cnt);
+        if(sem_lock_client != NULL)
+        {
+            status = sem_close(sem_lock_client);
+            if(status == -1)
+            {
+                debug_error("sem_close sem_lock_client failed");
+            }
+            sem_lock_client = NULL;
+        }
+        if(sem_lock_server != NULL)
+        {
+            status = sem_close(sem_lock_server);
+            if(status == -1)
+            {
+                debug_error("sem_close sem_lock_server failed");
+            }
+            sem_lock_server = NULL;
+        }
+        status = sem_unlink(SEM_NAME_CLIENT);
+        if(status == -1) 
+        {
+            debug_error("sem_unlink SEM_NAME_CLIENT failed\r\n" );
+        }
+        status = sem_unlink(SEM_NAME_SERVER);
+        if(status == -1) 
+        {
+            debug_error("sem_unlink SEM_NAME_SERVER failed\r\n" );
+        }
+        status = shmctl(shmid, IPC_RMID, NULL);
+        if (status < 0)
+        {
+            perror("shmctl del failed");
+        }
+        else
+        {
+            shmid = -1;
+        }
+    }
+    return;
+}
 
 void sem_client(int signal)
 {
@@ -72,25 +121,32 @@ void* sem_server(void *arg)
 int parent_init(void)
 {
     int ret = 0;
+    int sem_value = 0;
     sem_lock_client = sem_open(SEM_NAME_CLIENT, O_CREAT | O_EXCL, 0666, 0);
     sem_lock_server = sem_open(SEM_NAME_SERVER, O_CREAT | O_EXCL, 0666, 1);
     if (sem_lock_client == SEM_FAILED)
     {
+        debug_info("sem_lock_client exist\r\n");
         sem_lock_client = sem_open(SEM_NAME_CLIENT, 0);
         if (sem_lock_client == SEM_FAILED)
         {
             perror("sem_lock_client open failed\r\n");
             return -1;
         }
+        sem_getvalue(sem_lock_client, &sem_value);
+        debug_info("sem_lock_client:sem_value = %d\r\n", sem_value);
     }
     if (sem_lock_server == SEM_FAILED)
     {
+        debug_info("sem_lock_server exist\r\n");
         sem_lock_server = sem_open(SEM_NAME_SERVER, 0);
         if (sem_lock_server == SEM_FAILED)
         {
             perror("sem_lock_server open failed\r\n");
             return -1;
         }  
+        sem_getvalue(sem_lock_server, &sem_value);
+        debug_info("sem_lock_server:sem_value = %d\r\n", sem_value);
     }
 
     shmid = shmget(SHMKEY, 0, 0666);
@@ -116,18 +172,30 @@ int parent_init(void)
 int child_init(void)
 {
     int ret = 0;
-    sem_lock_client = sem_open(SEM_NAME_CLIENT, 0);
+    int sem_value = 0;
+    sem_lock_client = sem_open(SEM_NAME_CLIENT, O_CREAT | O_EXCL, 0666, 0);
+    sem_lock_server = sem_open(SEM_NAME_SERVER, O_CREAT | O_EXCL, 0666, 1);
     if (sem_lock_client == SEM_FAILED)
     {
-        perror("sem_lock_client open failed\r\n");
-        return -1;
+        sem_lock_client = sem_open(SEM_NAME_CLIENT, 0);
+        if (sem_lock_client == SEM_FAILED)
+        {
+            perror("sem_lock_client open failed\r\n");
+            return -1;
+        }
+        sem_getvalue(sem_lock_client, &sem_value);
+        debug_info("sem_lock_client:sem_value = %d\r\n", sem_value);
     }
-
-    sem_lock_server = sem_open(SEM_NAME_SERVER, 0);
     if (sem_lock_server == SEM_FAILED)
     {
-        perror("sem_lock_server open failed\r\n");
-        return -1;
+        sem_lock_server = sem_open(SEM_NAME_SERVER, 0);
+        if (sem_lock_server == SEM_FAILED)
+        {
+            perror("sem_lock_server open failed\r\n");
+            return -1;
+        }  
+        sem_getvalue(sem_lock_server, &sem_value);
+        debug_info("sem_lock_server:sem_value = %d\r\n", sem_value);
     }
 
     usleep(200 * 1000);
@@ -164,6 +232,8 @@ int main(void)
     { /* parent */
         parent_init();
         signal(SIGALRM, sem_client);
+        signal(SIGINT, eixt_handle);
+        signal(SIGTERM, eixt_handle);
 
         struct itimerval timer;
         timer.it_value.tv_sec = 0;
@@ -188,16 +258,42 @@ int main(void)
         }
         pthread_join(pid, NULL);
     }
-    sem_close(sem_lock_client);
-    sem_close(sem_lock_server);
-    sem_unlink(SEM_NAME_CLIENT);
-    sem_unlink(SEM_NAME_SERVER);
-    status = shmctl(shmid, IPC_RMID, NULL);
-    if (status < 0)
+    exit_cnt++;
+    printf("main exit exit_cnt = %d\r\n", exit_cnt);
+    if(sem_lock_client != NULL)
     {
-        perror("shmctl del failed");
+        status = sem_close(sem_lock_client);
+        if(status == -1)
+        {
+            debug_error("sem_close sem_lock_client failed\r\n" );
+        }
+        status = sem_unlink(SEM_NAME_CLIENT);
+        if(status == -1) 
+        {
+            debug_info("sem_unlink SEM_NAME_CLIENT failed\r\n" );
+        }
     }
-    printf("main exit\r\n");
+    if(sem_lock_server != NULL)
+    {
+        status = sem_close(sem_lock_server);
+        if(status == -1)
+        {
+            debug_error("sem_close sem_lock_server failed\r\n" );
+        }
+        status = sem_unlink(SEM_NAME_SERVER);
+        if(status == -1) 
+        {
+            debug_info("sem_unlink SEM_NAME_SERVER failed\r\n" );
+        }
+    }
+    if(shmid != -1)
+    {
+        status = shmctl(shmid, IPC_RMID, NULL);
+        if (status < 0)
+        {
+            perror("shmctl del failed");
+        }
+    }
     exit(0);
 }
 
